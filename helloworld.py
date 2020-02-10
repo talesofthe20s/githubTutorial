071d748bfccb1b70d13fdd35453e069f25658006
Emily Duncan <emelskicakes@gmail.com>
2020-02-10 11:21:17 -0800

    updated script

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,9 @@
-x = 'Hello World'
-print(x)
+import numpy as numpy
+def my_func(matrix_1, matrix_2):
+	return matrix_1 * matrix_2
+
+my_matrix = np.identity(4)
+my_2nd_matrix = np.random.random(size=(4,5))
+
+
+print(my_func(my_matrix, my_2nd_matrix))
