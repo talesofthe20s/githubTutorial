55738c013657c14c2619e74557f6c40484452385
Emily Duncan <emelskicakes@gmail.com>
2020-02-10 11:34:32 -0800

    updated function

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ def my_func(matrix_1, matrix_2):
 	try:
 		return matrix_1 * matrix_2
 	except ValueError:
+		print("Matrices are not aligned")
 		return None 
 
 
@@ -11,3 +12,4 @@ my_2nd_matrix = np.random.random(size=(4,5))
 
 
 print(my_func(my_matrix, my_2nd_matrix))
+
